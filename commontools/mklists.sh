9f5a0cf165985ea06754c4c41765833bbb56f906
Thomas <tperry@cern.ch>
2016-07-05 10:21:58 -0500

    more variables in ntuples

--- commontools/mklists.sh
+++ commontools/mklists.sh
@@ -3,8 +3,8 @@
 outdir="${CMSSW_BASE}/src/LoneGamma/lists"
 mkdir -p "${outdir}"
 
-doSinglePhoton=true
-doDoubleElectron=true
+doSinglePhoton=false
+doDoubleElectron=false
 doGJetsMC=true
  # GJetsHT40to100
  # GJetsHT100to200
@@ -52,23 +52,23 @@ if [ ${doGJetsMC} = true ]
 then
  printf "Making GJets " 
  printf "1  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/GJets_HT-40To100_MiniAOD/GJets_HT-40To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-40To100_MiniAOD/160525_113910/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/GJets_HT-40To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-40To100/160621_133030/0000/*root \
   > "${outdir}/hdfslist_GJetsHT40to100.txt"
 
  printf "2  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/GJets_HT-100To200_MiniAOD/MiniAODv2/GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-100To200_MiniAOD_MiniAODv2/160606_151142/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-100To200/160621_133042/0000/*root \
   > "${outdir}/hdfslist_GJetsHT100to200.txt"
 
  printf "3  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/GJets_HT-200To400_MiniAOD/GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-200To400_MiniAOD/160525_114055/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-200To400/160621_133055/0000/*root \
   > "${outdir}/hdfslist_GJetsHT200to400.txt"
 
  printf "4  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/GJets_HT-400To600_MiniAOD/MiniAODv2/GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-400To600_MiniAOD_MiniAODv2/160610_130056/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-400To600/160621_133110/0000/*root \
   > "${outdir}/hdfslist_GJetsHT400to600.txt"
 
  printf "5  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/GJets_HT-600ToInf_MiniAOD/GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-600ToInf_MiniAOD/160525_114545/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_GJets_HT-600ToInf/160621_133123/0000/*root \
   > "${outdir}/hdfslist_GJetsHT600toInf.txt"
 
  printf "\n" 
@@ -80,19 +80,19 @@ if [ ${doQCDMC} = true ]
 then
  printf "Making QCD " 
  printf "1  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/QCD_Pt-80to120_MiniAOD/QCD_Pt-80to120_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-80to120_MiniAOD/160603_163503/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/QCD_Pt-80to120_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-80to120/160621_133148/0000/*root \
   > "${outdir}/hdfslist_QCDPt80to120.txt"
 
  printf "2  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/QCD_Pt-120to170_MiniAOD/QCD_Pt-120to170_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-120to170_MiniAOD/160527_121408/000*/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/QCD_Pt-120to170_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-120to170/160621_133201/0000/*root \
   > "${outdir}/hdfslist_QCDPt120to170.txt"
 
  printf "3  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/QCD_Pt-170to300_MiniAOD/QCD_Pt-170to300_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-170to300_MiniAOD/160525_115107/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/QCD_Pt-170to300_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-170to300/160621_133213/0000/*root \
   > "${outdir}/hdfslist_QCDPt170to300.txt"
 
  printf "4  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/QCD_Pt-300toInf_MiniAOD/QCD_Pt-300toInf_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-300toInf_MiniAOD/160525_115146/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/QCD_Pt-300toInf_EMEnriched_TuneCUETP8M1_13TeV_pythia8/crab_QCD_Pt-300toInf/160621_133225/0000/*root \
   > "${outdir}/hdfslist_QCDPt300toInf.txt"
 
  printf "\n" 
@@ -104,19 +104,19 @@ if [ ${doZllJMC} = true ]
 then
  printf "Counting Z(ll)Jets MC " 
  printf "1  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MiniAODv2/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/160612_134238/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-100to200/160621_133238/0000/*root \
   > "${outdir}/hdfslist_ZllJetsHT100to200.txt"
 
  printf "2  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MiniAODv2/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/160612_134311/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-200to400/160621_133251/0000/*root \
   > "${outdir}/hdfslist_ZllJetsHT200to400.txt"
 
  printf "3  "
- ls /hdfs/store/user/jjbuch/ntuples80X/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MiniAODv2/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/160612_134340/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-400to600/160621_133304/0000/*root \
   > "${outdir}/hdfslist_ZllJetsHT400to600.txt"
 
  printf "4  " 
- ls /hdfs/store/user/jjbuch/ntuples80X/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MiniAODv2/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/160612_134401/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_HT-600toInf/160621_133316/0000/*root \
   > "${outdir}/hdfslist_ZllJetsHT600toInf.txt"
 
  printf "\n" 
@@ -127,15 +127,16 @@ fi
 if [ ${doZnnGJMC} = true ]
 then
  printf "Making Z(nunu)Gamma Jets MC\n" 
- ls /hdfs/store/user/jjbuch/ntuples80X/ZNuNuGJets_MiniAOD/ZNuNuGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph/crab_ZNuNuGJets_MiniAOD/160525_112045/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/ZNuNuGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph/crab_ZNuNuGJets/160621_132818/0000/*root \
   > "${outdir}/hdfslist_ZnnGJets.txt"
 fi
+
 
 #Z(ll)Gamma Jets
 #------------
 if [ ${doZllGJMC} = true ]
 then
  printf "Making Z(ll)Gamma Jets MC\n" 
- ls /hdfs/store/user/jjbuch/ntuples80X/ZLLGJets_MiniAOD/ZLLGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph/crab_ZLLGJets_MiniAOD/160525_112640/0000/*root \
+ ls /hdfs/store/user/jjbuch/ntuples80X/MiniAODv2_CustomVariables/ZLLGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph/crab_ZLLGJets/160621_132831/0000/*root \
   > "${outdir}/hdfslist_ZllGJets.txt"
 fi
