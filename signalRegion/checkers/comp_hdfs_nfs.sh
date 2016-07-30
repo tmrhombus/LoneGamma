244e7edae877ca1a65dee50c161625d181afdcde
Thomas <tperry@cern.ch>
2016-07-30 13:58:06 -0500

    added samples

--- signalRegion/checkers/comp_hdfs_nfs.sh
+++ signalRegion/checkers/comp_hdfs_nfs.sh
@@ -17,13 +17,36 @@ printf " \t\t /nfs \t /hdfs \t stauts \n"
 printf "%s " " -----------------------------------------"
 printf "%s \n" "-----------------------------------------"
 
-# "ZLLG" \
-# "ZJets100To200" \
-# "ZJets200To400" \
-# "ZJets400To600" \
-# "ZJets600ToInf" \
 for samplename in \
- "SinglePhoton"
+ "SinglePhoton"      \
+ "GJetsHT40to100"    \
+ "GJetsHT100to200"   \
+ "GJetsHT200to400"   \
+ "GJetsHT400to600"   \
+ "GJetsHT600toInf"   \
+ "QCDPt15to20"       \
+ "QCDPt20to30"       \
+ "QCDPt30to50"       \
+ "QCDPt50to80"       \
+ "QCDPt80to120"      \
+ "QCDPt120to170"     \
+ "QCDPt170to300"     \
+ "QCDPt300toInf"     \
+ "ZllGJets"          \
+ "ZnnGJets"          \
+ "ZllJetsHT100to200" \
+ "ZllJetsHT200to400" \
+ "ZllJetsHT400to600" \
+ "ZllJetsHT600toInf" \
+ "ZnnJetsHT100to200" \
+ "ZnnJetsHT200to400" \
+ "ZnnJetsHT400to600" \
+ "ZnnJetsHT600toInf" \
+ "WlnGJets"          \
+ "Wen"               \
+ "Wmn"               \
+ "Wtn"               \
+ "TTGJets"           
 do
 
  # analyzed
@@ -34,7 +57,8 @@ do
    analyzed_diff=$(($nfs_analyzed-$hdfs_analyzed))
    hdfstot=$((${hdfstot}+${hdfs_analyzed}))
    nfstot=$((${nfstot}+${nfs_analyzed}))
-  printf "  ${samplename}\t ${nfs_analyzed}\t ${hdfs_analyzed}\t ${analyzed_diff} incomplete analyzers\n"
+  printf "  %20s %6s %6s %s \n" "${samplename}" "${nfs_analyzed}" "${hdfs_analyzed}" "${analyzed_diff} incomplete analyzers"
+  #printf "  ${samplename}\t ${nfs_analyzed}\t ${hdfs_analyzed}\t ${analyzed_diff} incomplete analyzers\n"
  fi
 done # for samplename in "GJets_*" "SingleP"
 
@@ -48,3 +72,5 @@ printf "  Total: nfs: ${nfstot}  hdfs:  ${hdfstot}  left:  ${diftot}\n\n"
 #### grep -l "exited with status 0" /nfs_scratch/tperry/Earth_DataA_8TeVEle-Ele-PATData/*/*out > dataAEle_good.txt
 #### ls /nfs_scratch/tperry/Earth_DataA_8TeVEle-Ele-PATData/*/*out > dataAEle_all.txt
 #### grep -Fvf good_dataAEle.txt all_dataAEle.txt > dataAEle_bad.txt
+
+
