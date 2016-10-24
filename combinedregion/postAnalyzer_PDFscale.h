22aaf4c60cc20fcb74a33ec159e7cd105b83f19f
Thomas <tperry@cern.ch>
2016-10-24 04:08:42 -0500

    printouts of stats

--- combinedregion/postAnalyzer_PDFscale.h
+++ combinedregion/postAnalyzer_PDFscale.h
@@ -4,10 +4,19 @@
 
 #include "postAnalyzer_Gen.h"
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/moment.hpp>
+
 
 class postAnalyzer_PDFscale : public postAnalyzer_Gen {
 
  public:
+
+   std::vector<Float_t> vec_scales[7] ; //= std::vector<Float_t> ( 10, 0,0);
+   std::vector<float>   vec_pdfs  [7] ; //= std::vector<float>     ( 101, 0,0);
+
    TH1F h_pdfs_uncorret[7],
         h_scale_uncorret[7];
 
@@ -23,25 +32,15 @@ void postAnalyzer_PDFscale::InitPDFscale()
 
    std::cout<<"Initializing PDF/scale histograms"<<std::endl;
 
-   ptbins.clear();
-   ptbinnames.clear();
-
-   ptbins.push_back(175);
-   ptbins.push_back(190);
-   ptbins.push_back(250);
-   ptbins.push_back(400);
-   ptbins.push_back(700);
-   ptbins.push_back(1000);
-
-   ptbinnames.push_back("175to190");
-   ptbinnames.push_back("190to250");
-   ptbinnames.push_back("250to400");
-   ptbinnames.push_back("400to700");
-   ptbinnames.push_back("700to1000");
-   ptbinnames.push_back("175to1000");
-   ptbinnames.push_back("allpt");
-
    for(unsigned int i=0; i<ptbinnames.size(); ++i){
+
+     for( int q=0; q<10; ++q ){
+      vec_scales[i].push_back(0.0) ; 
+     }
+     for( int q=0; q<101; ++q ){
+      vec_pdfs[i]  .push_back(0.0) ;
+     }
+
 //     // set up names
      TString histname_pdfs_uncorret  = "h_pdfs_uncorret_"+ptbinnames[i];
      TString histname_scale_uncorret = "h_scale_uncorret_"+ptbinnames[i];
@@ -66,8 +65,9 @@ void postAnalyzer_PDFscale::InitPDFscale()
 
 Bool_t postAnalyzer_PDFscale::FillSigHistogramsPDFscale(int ptbin, double weight){
 
-  for( int i=1; i<102; ++i ){
-   h_pdfs_uncorret[ptbin].SetBinContent( i, ( h_pdfs_uncorret[ptbin].GetBinContent(i) + (weight*lheNormalizedWeights->at(110.+i)) ) ) ;
+  for( int i=0; i<101; ++i ){
+   h_pdfs_uncorret[ptbin].SetBinContent( i+1, ( h_pdfs_uncorret[ptbin].GetBinContent(i+1) + (weight*lheNormalizedWeights->at(110.+i)) ) ) ;
+   vec_pdfs[ptbin].at(i) = ( vec_pdfs[ptbin].at(i) + weight*lheNormalizedWeights->at(110.+i) );
   }
   h_pdfs_uncorret[ptbin].SetBinContent( 0 , h_pdfs_uncorret[ptbin].GetBinContent(0) + weight ) ;
 
@@ -81,6 +81,16 @@ Bool_t postAnalyzer_PDFscale::FillSigHistogramsPDFscale(int ptbin, double weight
   h_scale_uncorret[ptbin].SetBinContent( 7 , h_scale_uncorret[ptbin].GetBinContent(7) + weight*genWeight_QCDscale_muR0p5_muF1  ) ; 
   h_scale_uncorret[ptbin].SetBinContent( 8 , h_scale_uncorret[ptbin].GetBinContent(8) + weight*genWeight_QCDscale_muR0p5_muF2  ) ; 
   h_scale_uncorret[ptbin].SetBinContent( 9 , h_scale_uncorret[ptbin].GetBinContent(9) + weight*genWeight_QCDscale_muR0p5_muF0p5) ; 
+  vec_scales[ptbin].at(0) = vec_scales[ptbin].at(0)+weight;
+  vec_scales[ptbin].at(1) = vec_scales[ptbin].at(1)+weight*genWeight_QCDscale_muR1_muF1    ;
+  vec_scales[ptbin].at(2) = vec_scales[ptbin].at(2)+weight*genWeight_QCDscale_muR1_muF2    ;
+  vec_scales[ptbin].at(3) = vec_scales[ptbin].at(3)+weight*genWeight_QCDscale_muR1_muF0p5  ;
+  vec_scales[ptbin].at(4) = vec_scales[ptbin].at(4)+weight*genWeight_QCDscale_muR2_muF1    ;
+  vec_scales[ptbin].at(5) = vec_scales[ptbin].at(5)+weight*genWeight_QCDscale_muR2_muF2    ;
+  vec_scales[ptbin].at(6) = vec_scales[ptbin].at(6)+weight*genWeight_QCDscale_muR2_muF0p5  ;
+  vec_scales[ptbin].at(7) = vec_scales[ptbin].at(7)+weight*genWeight_QCDscale_muR0p5_muF1  ;
+  vec_scales[ptbin].at(8) = vec_scales[ptbin].at(8)+weight*genWeight_QCDscale_muR0p5_muF2  ;
+  vec_scales[ptbin].at(9) = vec_scales[ptbin].at(9)+weight*genWeight_QCDscale_muR0p5_muF0p5;
 
  return kTRUE;
 }
